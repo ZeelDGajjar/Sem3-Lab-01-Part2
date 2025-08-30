cb7852888aa435a3f51e57b3d0e1e61962f7c94a
zeelg <2474008@edu.vaniercollege.qc.ca>
2025-08-30 13:01:29 -0400

    Finished Essay class.

--- src/main/java/org/example/Essay.java
+++ src/main/java/org/example/Essay.java
@@ -1,4 +1,47 @@
 package org.example;
 
-public class Essay {
+public class Essay extends GradedActivity {
+    private double grammer;
+    private double spelling;
+    private double correctLength;
+    private double content;
+
+    public void setScore(double grammer, double spelling, double correctLength, double content) {
+        this.grammer = grammer;
+        this.spelling = spelling;
+        this.correctLength = correctLength;
+        this.content = content;
+    }
+
+    public void setGrammer(double grammer) {
+        this.grammer = grammer;
+    }
+
+    public void setSpelling(double spelling) {
+        this.spelling = spelling;
+    }
+
+    public void setCorrectLength(double correctLength) {
+        this.correctLength = correctLength;
+    }
+
+    public void setContent(double content) {
+        this.content = content;
+    }
+
+    public double getGrammer() {
+        return grammer;
+    }
+
+    public double getSpelling() {
+        return spelling;
+    }
+
+    public double getCorrectLength() {
+        return correctLength;
+    }
+
+    public double getScore() {
+        return this.grammer + this.spelling + this.correctLength + this.content;
+    }
 }
